5f239f7ed6382065a475944b6f1d99c608edeed4
Phill Hood <pvgh@outlook.com>
2021-01-11 17:12:37 -0500

    condensing pile div styling

--- src/client/components/game/Pile.jsx
+++ src/client/components/game/Pile.jsx
@@ -1,21 +1,13 @@
 import React from 'react';
-import { Wood, Blitz, Discard, Dutch } from './PileStyle';
+import { SinglePile } from './PileStyle';
 import Card from './Card.jsx';
-import { PILE_TYPES } from '../../constants';
-
-const { WOOD, BLITZ, DISCARD, DUTCH } = PILE_TYPES;
 
 const generatePile = (type, cards) => {
-  switch (type) {
-    case WOOD:
-      return <Wood cards={cards}>{displayCard(cards)}</Wood>;
-    case BLITZ:
-      return <Blitz cards={cards}>{displayCard(cards)}</Blitz>;
-    case DISCARD:
-      return <Discard cards={cards}>{displayCard(cards)}</Discard>;
-    case DUTCH:
-      return <Dutch cards={cards}>{displayCard(cards)}</Dutch>;
-  }
+  return (
+    <SinglePile type={type} cards={cards}>
+      {displayCard(cards)}
+    </SinglePile>
+  );
 };
 
 const displayCard = (cards) => {
--- src/client/components/game/PileStyle.js
+++ src/client/components/game/PileStyle.js
@@ -1,60 +1,23 @@
 import styled from 'styled-components';
-import { CARD_DIMENSIONS } from '../../constants';
+import { PILE_TYPES } from '../../constants';
 
-export const Discard = styled.div`
+const { WOOD, BLITZ, DISCARD } = PILE_TYPES;
+
+export const SinglePile = styled.div`
   & {
     display: flex;
     border: 2px solid black;
     border-radius: 10px;
-    background: lime;
-  }
-  &:before {
-    content: '';
-    display: block;
-    height: 0;
-    width: 0;
-    padding-bottom: calc(14 / 10 * 100%);
-  }
-`;
-
-export const Wood = styled.div`
-  & {
-    display: flex;
-    border: 2px solid black;
-    border-radius: 10px;
-    background: brown;
-  }
-  &:before {
-    content: '';
-    display: block;
-    height: 0;
-    width: 0;
-    padding-bottom: calc(14 / 10 * 100%);
-  }
-`;
-
-export const Blitz = styled.div`
-  & {
-    display: flex;
-    border: 2px solid black;
-    border-radius: 10px;
-    background: gold;
-  }
-  &:before {
-    content: '';
-    display: block;
-    height: 0;
-    width: 0;
-    padding-bottom: calc(14 / 10 * 100%);
-  }
-`;
-
-export const Dutch = styled.div`
-  & {
-    display: flex;
-    border: 2px solid black;
-    border-radius: 10px;
-    background: white;
+    background: ${({ type }) => {
+      switch (type) {
+        case WOOD:
+          return 'gray';
+        case BLITZ:
+          return 'gold';
+        case DISCARD:
+          return 'lime';
+      }
+    }};
   }
   &:before {
     content: '';
