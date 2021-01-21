d1a7a58880b58d96e7db47204b14013585aba04d
Phill Hood <pvgh@outlook.com>
2021-01-21 13:58:01 -0500

    removed auth0

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Auth0Provider } from '@auth0/auth0-react';
+// import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
 
-ReactDOM.render(
-  <Auth0Provider domain="" clientId="" redirectUri={window.location.origin}>
-    <App />
-  </Auth0Provider>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
